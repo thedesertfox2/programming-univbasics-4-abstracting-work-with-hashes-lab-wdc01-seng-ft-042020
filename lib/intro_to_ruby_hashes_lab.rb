0d3e350ee1f24af5cae1c32e2d015e659a1a1e30
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-10 14:31:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -16,6 +16,7 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
     if hash[key]
       hash[key] = hash[key] + 1
+      key =+ 1
   
     else
   end
